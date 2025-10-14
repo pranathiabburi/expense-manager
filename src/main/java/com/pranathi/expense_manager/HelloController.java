a1e2357009a80f610bd303a0a19671aedbe80be2
Pranathi Abburi <Pranathi@Macbook.local>
2025-10-14 11:43:30 -0700

    Day 1 - setup and hello endpoint

---
+++ src/main/java/com/pranathi/expense_manager/HelloController.java
@@ -0,0 +1,13 @@
+package com.pranathi.expense_manager;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Expense Manager is running!";
+    }
+}
